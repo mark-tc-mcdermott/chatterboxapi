d3a2f1d12038fba2bcd0567c24f0d8c6fe22b3e8
mtcmcdermott <mark.tc.mcdermott@gmail.com>
2016-10-10 23:43:37 -0500

    Add DTOs so we don't directly have the user access the database

--- DTO/LoginDTO.cs
+++ DTO/LoginDTO.cs
@@ -2,21 +2,14 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace ChatterboxApi.DAL.Models
+namespace ChatterboxApi.DTO
 {
-    public class User
-    {
-        [Key]
-        public int UserId { get; set; }
-        public string name { get; set; }
-
+    public class LoginDTO
+    {   
         [Required]
         public string email { get; set; }
         [Required]
         [DataType(DataType.Password)]
         public string password { get; set; }
-
-
-        public System.DateTime CreationDate { get; set;}
     }
 }
--- DTO/RegisterDTO.cs
+++ DTO/RegisterDTO.cs
@@ -2,21 +2,18 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace ChatterboxApi.DAL.Models
+namespace ChatterboxApi.DTO
 {
-    public class User
+    public class RegisterDTO 
     {
-        [Key]
-        public int UserId { get; set; }
+        [Required]
         public string name { get; set; }
-
+        [DataType(DataType.Password)]
+        public string confirm { get; set; }
         [Required]
         public string email { get; set; }
         [Required]
         [DataType(DataType.Password)]
         public string password { get; set; }
-
-
-        public System.DateTime CreationDate { get; set;}
     }
 }
