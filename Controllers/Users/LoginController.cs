28aa2e09c4f7531ec661a64c06fdda5cf9f45ef5
mtcmcdermott <mark.tc.mcdermott@gmail.com>
2016-10-11 22:58:55 -0500

    Add in the login functionaliry

--- Controllers/Users/LoginController.cs
+++ Controllers/Users/LoginController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ChatterboxApi.DTO;
+using ChatterboxApi.DAL.Models;
+using Chatterbox.DAL;
+using ChatterboxApi.DAL;
 using System.Net.Http;
 
 namespace ChatterboxAppApi.Controllers
@@ -11,17 +14,33 @@ namespace ChatterboxAppApi.Controllers
     [Route("users/login")]
     public class LoginController : Controller
     {
+        private ChatterboxRepository cbRep;
+
+        public LoginController()
+        {
+            this.cbRep = new ChatterboxRepository(new ChatterboxContext());
+        }
+
         [HttpPost]
         public ActionResult Post([FromBody] LoginDTO payload)
         {
-          if (ModelState.IsValid)
-          {
-              return Ok();
-          }
-          else
-          {
-              return  BadRequest();
-          }
+            if (ModelState.IsValid && this.cbRep.PasswordMatches(payload.email, payload.password) )
+            {  
+                User user = this.cbRep.GetUserByEmail(payload.email);
+                var registerdto = new UserResponseDTO()
+                {
+                    id = user.UserId,
+                    email= user.Email,
+                    name= user.Name
+                };
+                registerdto.setToken();
+                
+                return Json(new { success=true, data= registerdto});
+            }
+            else
+            {
+                return  BadRequest();
+            }
         }
     }
 }
