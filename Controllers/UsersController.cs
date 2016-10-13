a79d18efea69b64bd2398ad761ee62026098f684
mtcmcdermott <mark.tc.mcdermott@gmail.com>
2016-10-12 20:05:08 -0500

    Implement put and clean up code for getting user

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,25 +1,70 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNet.Authorization;
+using ChatterboxApi.DAL;
+using ChatterboxApi.DAL.Models;
+using ChatterboxApi.DTO;
+using ChatterboxAPI.Provider;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 namespace ChatterboxAppApi.Controllers
 {
-    [Authorize]
     [Route("users")]
     public class UsersController : Controller
     {
-        [HttpGet("me")]
-        public void Get()
-        {
+        private ChatterboxRepository cbRep;
 
+        public UsersController()
+        {
+             this.cbRep = new ChatterboxRepository(new ChatterboxContext());
+        }
+
+        [HttpGet("me")]
+        public ActionResult Get()
+        {            
+            User userObject = AuthProvider.validateToken(Request.Headers["Authorization"]);
+            if (userObject != null)
+            { 
+                return Json(new { success=true, data= new UserResponseDTO(userObject)});
+            } 
+            else
+            {
+                return BadRequest();
+            }
         }
         
         [HttpPut("me")]
-        public void Put(int id, [FromBody]string value)
+        public ActionResult Put([FromBody]JObject value)
         {
+            
+            User userObject =  AuthProvider.validateToken(Request.Headers["Authorization"]);
+            if (userObject != null)
+            {
+                if (value.Value<string>("name") != null)
+                {
+                    userObject.Name = value.Value<string>("name");
+                }
+                if (value.Value<string>("password") != null && value.Value<string>("password") == value.Value<string>("password") )
+                {
+                    userObject.Password = value.Value<string>("password");
+                }
+                else if(value.Value<string>("password") != null)
+                {
+                    return BadRequest();
+                }
+                if (value.Value<string>("email") != null) {
+                    userObject.Email = value.Value<string>("email");
+                }
+                this.cbRep.UpdateUser(userObject);
+                
+                return Json(new { success=true, data=new UserResponseDTO(userObject)});
+            } 
+            else
+            {
+                return BadRequest();
+            }
 
         }
     }
