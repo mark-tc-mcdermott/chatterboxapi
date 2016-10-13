4437e5f8ac04e4a4f411bf6f183ecc75b7cf66c2
mtcmcdermott <mark.tc.mcdermott@gmail.com>
2016-10-12 20:04:37 -0500

    Implement chat saving and fetching

--- Controllers/ChatController.cs
+++ Controllers/ChatController.cs
@@ -1,24 +1,78 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNet.Authorization;
+using ChatterboxApi.DAL;
+using ChatterboxApi.DAL.Models;
+using ChatterboxApi.DTO;
+using ChatterboxAPI.Provider;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace ChatterboxAppApi.Controllers
 {
-    [Authorize]
-    [Route("chat")]
+    [Route("chats")]
     public class ChatController : Controller
     {
-        [HttpGet]
-        public void Get()
+        private ChatterboxRepository cbRep;
+
+        public ChatController()
         {
+             this.cbRep = new ChatterboxRepository(new ChatterboxContext());
+        }
+        [HttpGet]
+        public ActionResult Get([FromQuery] QueryDTO query)
+        {
+
+            string authorization = Request.Headers["Authorization"];
+            
+            User userObject = AuthProvider.validateToken(authorization);
+            if (userObject != null)
+            {
+                List<Chat> listOChats =  this.cbRep.FindChatsByName(query.q);
+                List<ChatDTO> dtoList = new List<ChatDTO>();
+                foreach (Chat chat_ref in listOChats)
+                {
+                    var last_message = this.cbRep.FindLastChatMessage(chat_ref.ChatId);
+
+                    MessageDTO message_dto = null;
+                    if (last_message != null)
+                    {
+                        message_dto = new MessageDTO(last_message);
+                    }
+                    dtoList.Add(new ChatDTO(chat_ref, message_dto));
+                }
+                
+                return Json(new { success=true, data= dtoList});
+            } 
+            else
+            {
+                return BadRequest();
+            }
+            
         }
         
         [HttpPost]
-        public void Post()
-        {
+        public ActionResult Post([FromBody] JObject name)
+        {   
+            User userObject = AuthProvider.validateToken(Request.Headers["Authorization"]);
+            if (userObject != null)
+            {
+                Chat chat_ref = new Chat()
+                {
+                    Name = name.Value<string>("name"),
+                    UserId = userObject.UserId,
+                    CreationDate = System.DateTime.Now
+                };
 
+                this.cbRep.InsertChat(chat_ref);
+ 
+                return Json(new { success=true, data= new ChatDTO(chat_ref, null)});
+            } 
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
